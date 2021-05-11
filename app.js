27c8a283183e344959fc2c14d1cbb665e940cd67
reedbrownart <reedb@DESKTOP-COU5FD4>
2021-05-11 12:45:12 -0400

    second push, moving to develop oops

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const Express = require('express');
 const app = Express();
+const dbConnection = require('./db');
+
+app.use(Express.json());
 
 const controllers = require('./controllers');
 app.use("/user", controllers.userController);
@@ -9,6 +12,13 @@ app.use('/test', (req, res) => {
     res.send('This is a test endpoint');
 })
 
-app.listen(3000, () => {
-    console.log(`[Server]: App is listening on 3000`);
-});
+dbConnection.authenticate()
+    .then(() => dbConnection.sync())
+    .then(() => {
+        app.listen(3000, () => {
+            console.log(`[Server]: App is listening on 3000`);
+        });
+    })
+    .catch((err) => {
+        console.log(`[Server]: Server crashed. Error: ${err}`);
+    })
--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,12 +1,38 @@
 const Express = require('express');
 const router = Express.Router();
+const { UserModel } = require('../models');
+const { UniqueConstraintError } = require('sequelize/lib/errors');
 
 router.get('/usertest', (req, res) => {
     res.send('you have reached the user endpoint');
 })
 
-router.post('/register', (req, res) => {
-    res.send('you have reached the register user endpoint');
+router.post('/register', async (req, res) => {
+
+    let { firstName, lastName, email, password } = req.body;
+    try{
+        let User = await UserModel.create({
+            firstName,
+            lastName,
+            email,
+            password
+        })
+        
+        res.status(201).json({
+            message: "User successfully registered",
+            user: User
+        })
+    } catch (err) {
+        if (err instanceof UniqueConstraintError) {
+            res.status(409).json({
+                message: "Email already in use"
+            })
+        } else {
+            res.status(500).json({
+                message: "Failed for some unknown reason"
+            })
+        }
+    }
 })
 
 router.post('/login', (req, res) => {
