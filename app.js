b2f9e1d3be93e6d2c36eb8d91ff6f986e353cc2b
reedbrownart <reedb@DESKTOP-COU5FD4>
2021-05-18 12:07:37 -0400

    heroku final i hope

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use('/test', (req, res) => {
 })
 
 dbConnection.authenticate()
-    .then(() => dbConnection.sync({ force: true }))
+    .then(() => dbConnection.sync())
     .then(() => {
         app.listen(process.env.PORT, () => {
             console.log(`[Server]: App is listening on ${process.env.PORT}`);
